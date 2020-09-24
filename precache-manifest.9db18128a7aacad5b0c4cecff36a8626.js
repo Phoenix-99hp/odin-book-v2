47389afb410962d6cd64da2a6c767d7ad864b6e6
Phoenix <drake.mcgarrah@gmail.com>
2020-09-24 15:11:15 -0500

    Updates

---
+++ precache-manifest.9db18128a7aacad5b0c4cecff36a8626.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29f83deeb3f8d1ff1bc2ce89b950cecc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "315c6c5e70a13e620ec2",
+    "url": "/static/css/main.6356522f.chunk.css"
+  },
+  {
+    "revision": "6de016ff1936bde7b42e",
+    "url": "/static/js/2.367f5981.chunk.js"
+  },
+  {
+    "revision": "ddc260dea7c44235f4d8e919b6e1dd91",
+    "url": "/static/js/2.367f5981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "315c6c5e70a13e620ec2",
+    "url": "/static/js/main.8a749ed0.chunk.js"
+  },
+  {
+    "revision": "f1cb5f929502bb0afd14",
+    "url": "/static/js/runtime-main.cda4822f.js"
+  },
+  {
+    "revision": "a06a3248f173f51bcbbcdd434a1de129",
+    "url": "/static/media/white-waves.a06a3248.png"
+  }
+]);
