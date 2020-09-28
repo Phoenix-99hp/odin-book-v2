81c4439269c9206054f60b64a9dac7215eb9cce4
Phoenix <drake.mcgarrah@gmail.com>
2020-09-28 15:06:47 -0500

    Changed timing

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -24,7 +24,7 @@ const Layout = ({ children }) => {
 		setTimeout(() => {
 			dispatch(setTransitioning(false));
 			setShowTransitionPage(true);
-		}, 2000);
+		}, 3000);
 	}, [shouldTransition]);
 
 	useEffect(() => {
--- src/components/LoginLayout/LoginLayout.js
+++ src/components/LoginLayout/LoginLayout.js
@@ -24,7 +24,7 @@ const LoginLayout = ({ children }) => {
 		setTimeout(() => {
 			dispatch(setTransitioning(false));
 			setShowTransitionPage(true);
-		}, 2000);
+		}, 3000);
 	}, [shouldTransition]);
 
 	useEffect(() => {
